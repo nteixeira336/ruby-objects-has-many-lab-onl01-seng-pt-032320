ce0af1d77c007da53f35db180b02c2eecfc5f012
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-04-28 14:30:21 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author 
+  
+  @@all = []
+  @@post_count = 1 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end 
+  
+  def posts 
+    Post.all.select do |post|
+     post.author = self 
+    end 
+  end 
+  
+  def add_post(post)
+    self.posts << post 
+    post.author == self 
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post 
+    post.author = self 
+  end 
+  
+  def self.post_count 
+    
+end 
